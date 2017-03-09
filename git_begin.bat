da1ee091e0d4fc0f5f97c07bdc33cb32bb234354
Litvinov Mikhail <sid-4d@inbox.ru>
2017-03-09 19:57:02 +0400

    Lets go to diner

--- git_begin.bat
+++ git_begin.bat
@@ -3,9 +3,17 @@ REM git init
 REM git status
 REM edit 1.txt
 REM git status
-git add .
+REM git add .
+REM git status
+REM git commit -m "Add many files"
+REM git log 
+REM git log --help
+REM edit 1.txt
+REM git status
+echo  "  отмена изменений локальных до индексации"
+git checkout 1.txt
 
 echo Press ENTER to execute the command
-Pause >nul
+Pause>nul
 
 

119099138acb418352f7b049538a90910b0396f5
Litvinov Mikhail <sid-4d@inbox.ru>
2017-03-09 19:35:48 +0400

    Add many files

---
+++ git_begin.bat
@@ -0,0 +1,11 @@
+cls
+REM git init
+REM git status
+REM edit 1.txt
+REM git status
+git add .
+
+echo Press ENTER to execute the command
+Pause >nul
+
+
---
+++ temp.bat
@@ -0,0 +1,19 @@
+cls
+git config -l 
+echo Press ENTER to execute the command
+Pause >nul
+
+
+cls
+git config --global user.name "Litvinov Mikhail"
+git config -l 
+echo Press ENTER to execute the command
+Pause >nul
+
+
+cls
+git config --global user.email "sid-4d@inbox.ru"
+git config -l 
+echo Press ENTER to execute the command
+Pause >nul
+
